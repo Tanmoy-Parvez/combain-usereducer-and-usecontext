9919ce8bbe3975887289c73051a6880787a77e06
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-12-06 16:51:56 +0600

    made a counter using useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const App = () => {
+
+  const initialState = 0;
+
+  const reducer = (state, action) => {
+
+    switch (action.type) {
+      case "INCREMENT":
+        return state + action.payload;
+
+      case "DECREMENT":
+        return state - action.payload;
+
+      default:
+        return state;
+    }
+
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+
   return (
     <div className='flex h-[100vh] items-center justify-center'>
       <div className="card w-96 bg-neutral text-neutral-content">
         <div className="card-body items-center text-center">
-          <h2 className="card-title">0</h2>
+          <h2 className="card-title">{state}</h2>
           <div className="card-actions justify-end">
-            <button className="btn btn-success">Increment</button>
-            <button className="btn btn-error">Decrement</button>
+            <button className="btn btn-success" onClick={() => dispatch({ type: "INCREMENT", payload: 5 })}>Increment</button>
+            <button className="btn btn-error" onClick={() => dispatch({ type: "DECREMENT", payload: 5 })}>Decrement</button>
           </div>
         </div>
       </div>
